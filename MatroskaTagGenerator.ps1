f99217aeba2052de406e9e22953b9c8dd062a988
patrickenfuego <patrick.m.kelly1989@gmail.com>
2021-12-27 19:22:05 -0500

    Update error handling & console output

--- MatroskaTagGenerator.ps1
+++ MatroskaTagGenerator.ps1
@@ -77,12 +77,12 @@
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $true, Position = 0)]
-    [Alias('P')]
+    [Alias('P', 'File')]
     [string]$Path,
 
     [Parameter(Mandatory = $false, Position = 1)]
     [Alias('Key')]
-    [string]$APIKey,
+    [string]$APIKey = '9062555f293fab25e1aaffe822b93bb4',
 
     [Parameter(Mandatory = $false, Position = 2)]
     [Alias('T')]
@@ -106,21 +106,23 @@ param (
 )
 
 #########################################################
-# Define Variables                                      #                                           
+# Define Variables & Sanitize Input                     #                                           
 #########################################################
-
-#Sanitize title if not passed via parameter
-if (!$PSBoundParameters['Title']) {
-    $Title = (Split-Path -Path $Path -Leaf) -replace '\..*', ''
-
-    if ($Title -match "^(?<title>.*).*(?<year>\d{4}).*\d+p") {
-        $Title = ($Matches.title -replace '\.|_', ' ').Trim()
-    }
-}
 
 #Console colors
 $progressColors = @{ForegroundColor = 'Green'; BackgroundColor = 'Black' }
 $warnColors = @{ForegroundColor = 'Yellow'; BackgroundColor = 'Black' }
+$dividerColor = @{ ForegroundColor = 'DarkMagenta'; BackgroundColor = 'Black' }
+
+$banner = @'
+#     #                                                    #######                   #####                                                         
+##   ##   ##   ##### #####   ####   ####  #    #   ##         #      ##    ####     #     # ###### #    # ###### #####    ##   #####  ####  #####  
+# # # #  #  #    #   #    # #    # #      #   #   #  #        #     #  #  #    #    #       #      ##   # #      #    #  #  #    #   #    # #    # 
+#  #  # #    #   #   #    # #    #  ####  ####   #    #       #    #    # #         #  #### #####  # #  # #####  #    # #    #   #   #    # #    # 
+#     # ######   #   #####  #    #      # #  #   ######       #    ###### #  ###    #     # #      #  # # #      #####  ######   #   #    # #####  
+#     # #    #   #   #   #  #    # #    # #   #  #    #       #    #    # #    #    #     # #      #   ## #      #   #  #    #   #   #    # #   #  
+#     # #    #   #   #    #  ####   ####  #    # #    #       #    #    #  ####      #####  ###### #    # ###### #    # #    #   #    ####  #    # 
+'@
 
 #########################################################
 # Function Definitions                                  #                                           
@@ -131,14 +133,21 @@ function Get-ID {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNullOrEmpty()]
         [string]$Title,
 
         [Parameter(Mandatory = $true, Position = 1)]
+        [ValidateNotNullOrEmpty()]
         [string]$APIKey
     )
 
+    Write-Host "Requesting TMDB ID for '$Title'..."
     $query = Invoke-RestMethod -Uri "https://api.themoviedb.org/3/search/movie?api_key=$($APIKey)&query=$($Title)" -Method GET
-    $id = $query.results[0].id
+    if ($id = $query.results[0].id) {
+        Write-Host "ID successfully retrieved! ID: " -NoNewline
+        Write-Host $id @progressColors
+        Write-Host "---------------------------------------------" @dividerColor
+    }
 
     return [int]$id
 }
@@ -147,70 +156,117 @@ function Get-ID {
 function Get-Metadata {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNullOrEmpty()]
         [string]$Id,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $true, Position = 1)]
         [string]$APIKey
     )
 
-    $customProps = @{}
-
-    #Pull general info including IMDb ID
-    $genQuery = Invoke-RestMethod -Uri "https://api.themoviedb.org/3/movie/$($id)?api_key=$($APIKey)" -Method GET
-    $imdbID = $genQuery | Select-Object -ExpandProperty imdb_id
-
     #Create base object
     $obj = @{
-        'IMDb' = $imdbID
         'TMDB' = $Id
     }
 
-    #Search for custom properties and add them if found
-    if ($Properties) {
+    #Pull general info including IMDb ID
+    if ('IMDbID' -notin $SkipProperties) {
+        Write-Host "Requesting IMDB ID..."
+        $genQuery = Invoke-RestMethod -Uri "https://api.themoviedb.org/3/movie/$($id)?api_key=$($APIKey)" -Method GET
+        if ($imdbID = $genQuery | Select-Object -ExpandProperty imdb_id) {
+            Write-Host "IMDb ID successfully retrieved! ID: " -NoNewline
+            Write-Host $imdbID @progressColors
+
+            $obj['IMDb'] = $imdbID
+        }
+        else {
+            Write-Warning "Failed to retrieve IMDb ID. Property will be skipped"
+        }
+    }
+    else { Write-Host "Skipping IMDb ID..." @warnColors  }
+
+    Write-Host "---------------------------------------------" @dividerColor
+
+    #Pull cast/crew data
+    $credits = Invoke-RestMethod -Uri "https://api.themoviedb.org/3/movie/$($Id)/credits?api_key=$($APIKey)" -Method GET
+
+    # Get cast members
+    if ('Cast' -notin $SkipProperties) {
+        Write-Host "Requesting cast metadata..."
+        if ($cast = $credits.cast | Select-Object -ExpandProperty name -First 5 -Unique) {
+            Write-Host "Cast metadata successfully retrieved! Cast info: " -NoNewline
+            Write-Host $($cast -join ', ') @progressColors
+
+            $obj['Cast'] = $cast
+        }
+        else {
+            Write-Warning "Failed to retrieve cast metadata. Property will be skipped"
+        }
+    }
+    else { Write-Host "Skipping Cast..." @warnColors }
+
+    Write-Host "---------------------------------------------" @dividerColor
+
+    # Get writers
+    if ('Writers' -notin $SkipProperties) {
+        Write-Host "Requesting writers metadata..."
+        $writers = $credits.crew | Where-Object { $_.department -eq "Writing" } | 
+            Select-Object name, job -First 3 -Unique
+        [array]$writerArray += $writers.ForEach({ "$($_.name) ($($_.job))" })
+        if ($writerArray) {
+            Write-Host "Cast metadata successfully retrieved! Writers info: " -NoNewline
+            Write-Host $($writerArray -join ', ') @progressColors
+            
+            $obj['Written By'] = $writerArray
+        }
+        else {
+            Write-Warning "Failed to retrieve Writers metadata. Property will be skipped"
+        }
+    }
+    else { Write-Host "Skipping Writers..." @warnColors }
+        
+    Write-Host "---------------------------------------------" @dividerColor
+    
+    # Get directors
+    if ('Directors' -notin $SkipProperties) {
+        Write-Host "Requesting directors metadata..."
+        $directors = $credits.crew | Where-Object { $_.department -eq "Directing" -and $_.job -eq "Director" } | 
+            Select-Object -ExpandProperty name -First 2 -Unique
+        if ($directors) {
+            Write-Host "Directors metadata successfully retrieved! Directors info: " -NoNewline
+            Write-Host $($directors -join ', ') @progressColors
+
+            $obj['Directed By'] = $directors
+        }
+        else {
+            Write-Warning "Failed to retrieve Directors metadata. Property will be skipped"
+        }
+    }
+    else { Write-Host "Skipping Directors..." @warnColors }
+
+    Write-Host "---------------------------------------------" @dividerColor
+   
+     #Search for custom properties and add them if found
+     if ($Properties) {
+        Write-Host "Searching for additional user-defined properties..."
         Write-Verbose "Property passed: $($Properties -join ', ')"
         foreach ($prop in $Properties) {
             if (![string]::IsNullOrEmpty($genQuery.$prop)) {
                 $prop = (Get-Culture).TextInfo.ToTitleCase($prop)
-                Write-Verbose "$prop was found"
-                $customProps.Add($prop, $genQuery.$prop)
+                #check for duplicate key before adding
+                if (!$obj.ContainsKey($prop)) {
+                    $obj.Add($prop, $genQuery.$prop)
+
+                    Write-Host "---------------------------------------------" @dividerColor
+                    Write-Host "$prop metadata successfully retrieved! $prop info: " -NoNewline
+                    Write-Host $genQuery.$prop @progressColors
+                }
+                else { Write-Warning "$prop is a duplicate key. Property will be skipped"}
             }
-            else { Write-Verbose "$prop property not found on return object" }
-        }
-    }
+            else { Write-Host "$prop property was not found on return object" @warnColors }
 
-    #Pull cast/crew data
-    $credits = Invoke-RestMethod -Uri "https://api.themoviedb.org/3/movie/$($Id)/credits?api_key=$($APIKey)&language=en-US" -method GET
-
-    # Get cast members
-    if ('Cast' -notin $SkipProperties) {
-        $cast = $credits.cast | Select-Object -ExpandProperty name -First 5 -Unique
-        $obj['Cast'] = $cast
-    }
-
-    # Get writers
-    if ('Writers' -notin $SkipProperties) {
-        $writers = $credits.crew | Where-Object { $_.department -eq "Writing" } | 
-            Select-Object name, job -First 3 -Unique
-        [array]$writerArray += $writers.ForEach({ "$($_.name) ($($_.job))" })
-        $obj['Written By'] = $writerArray
-    }
-    
-    # Get directors
-    if ('Directors' -notin $SkipProperties) {
-        $directors = $credits.crew | Where-object { $_.department -eq "Directing" -and $_.job -eq "Director" } | 
-            Select-Object -ExpandProperty name -First 2 -Unique
-        $obj['Directed By'] = $directors
-    }
-   
-    #If custom properties were found, ensure no duplicate keys exist and add them to return object
-    if ($customProps.Count -gt 0) {
-        foreach ($key in $customProps.Keys) {
-            if ($obj.ContainsKey($key)) {
-                Write-Warning "Duplicate key found. Value will be skipped"
-            }
-            else {
-                $obj[$key] = $customProps.$key
+            if ($prop -eq $Properties[-1]) {
+                Write-Host "---------------------------------------------" @dividerColor
             }
         }
     }
@@ -219,10 +275,11 @@ function Get-Metadata {
 } 
 
 #Generates the XML file
-function New-XMLFile {
+function New-XMLTagFile {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNullOrEmpty()]
         [object]$Metadata,
 
         [Parameter(Mandatory = $true, Position = 1)]
@@ -265,17 +322,41 @@ else {
 }
 
 if ((Test-Path -Path $outXML) -and !$PSBoundParameters['AllowClobber']) {
-    Write-Host "file already exists. Skipping creation`n" @warnColors
+    Write-Host "<$outXML> already exists. To proceed, delete the file or use the -AllowClobber parameter to overwrite`n" @warnColors
     exit 0
 }
+
+Write-Host "$banner`n`n" @dividerColor
+
+#Sanitize title if not passed via parameter
+if (!$PSBoundParameters['Title']) {
+    $leafBase = (Split-Path -Path $Path -Leaf) -replace '\..*', ''
+
+    #Try to sanitize input title
+    $Title = switch -Regex ($leafBase) {
+        "^(?<title>[^(]+)(?=.*\()"            { ($Matches.title -replace '\.|_', ' ').Trim(); break }
+        "^(?<title>.*).*(?<year>\d{4}).*\d+p" { ($Matches.title -replace '\.|_', ' ').Trim(); break }
+        default                               { 'Undefined' }
+    }
+    if ($Title -ne 'Undefined') {
+        Write-Host "Search title: " -NoNewline
+        Write-Host $Title @progressColors
+    }
+    else { 
+        Write-Warning "Could not sanitize input title, query may fail. Consider using the -Title parameter instead"
+        $Title = $leafBase -replace '\.|_', ' '
+        Write-Host "Title is: " -NoNewline
+        Write-Host $Title @progressColors
+    }
+}
+
 
 #Try to retrieve metadata. Catch and display a variety of potential errors
 try {
     [int]$id = Get-ID -Title $Title -APIKey $APIKey -ErrorAction Stop
-    $movieObj = Get-Metadata -Id $id -APIKey $APIKey
+    $movieObj = Get-Metadata -Id $id -APIKey $APIKey -ErrorAction Stop
     
     Write-Verbose "Object output:`n`n$($movieObj | Out-String)"
-    
 }
 catch {
     if (!$id) {
@@ -283,7 +364,7 @@ catch {
         $testQuery = Get-ID -Title $testTitle -APIKey $APIKey
         if ($testQuery) {
             $params = @{
-                Message           = "Return ID is empty, but the API endpoint is reachable using:`n`nKey:`t`t'$APIKey'`nTest Query:`t'$testTitle'"
+                Message           = "Return ID is empty, but the API endpoint is reachable using:`n`nKey: '$APIKey'`nTest Query: '$testTitle'`n"
                 RecommendedAction = "Verify that the target title is correct"
                 Category          = 'InvalidArgument'
                 CategoryActivity  = "TMDB Identifier Retrieval"
@@ -307,7 +388,7 @@ catch {
     elseif (!$movieObj) {
         $params = @{
             Message           = "REST API call returned an empty object"
-            RecommendedAction = "Verify that the IMDb API is online and functioning"
+            RecommendedAction = "Verify that the TMDB API is online and functioning"
             Category          = 'ResourceUnavailable'
             CategoryActivity  = "Metadata Request"
             TargetObject      = $movieObj
@@ -316,19 +397,21 @@ catch {
         Write-Error @params
     }
     Write-Host
-    throw "Failed to retrieve metadata. Exiting script"
+    Write-Error "Failed to retrieve metadata. Exiting script"
+    exit 1
 }
 
 #Try to create XML file
 try {
-    New-XMLFile -Metadata $movieObj -OutputFile $outXML
+    New-XMLTagFile -Metadata $movieObj -OutputFile $outXML -ErrorAction Stop
 }
 catch {
     if ((Test-Path $outXML) -and (Get-Item $outXML).Length -gt 0) {
-        Write-Warning "XML tag file was successfully generated, but an exception occurred: $($_.Exception.Message)"
+        Write-Warning "XML tag file was successfully generated, but an Exception occurred: $($_.Exception.Message)"
     }
     else {
-        throw "Failed to generate XML file. Exception: $($_.Exception.Message)"
+        Write-Error "Failed to generate XML file. Exception: $($_.Exception.Message)"
+        exit 1
     }
 }
 
@@ -339,5 +422,9 @@ if ((Get-Command 'mkvpropedit') -and !$PSBoundParameters['NoMux'] -and $Path.End
 }
 elseif (!(Get-Command 'mkvpropedit') -and !$PSBoundParameters['NoMux']) {
     Write-Host "mkvpropedit not found in PATH. Add the tag file to the container manually" @warnColors
+    exit 0
+}
+else {
+    Write-Host "Success! Exiting script" @progressColors
     exit 0
 }
